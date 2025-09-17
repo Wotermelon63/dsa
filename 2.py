c015374e5bc25d27656f73975cb287e4a9862a68
Игнатьев иу10-36 <wotermelon63@gmail.com>
2025-09-17 23:06:32 +0300

    добавил графики

--- 2.py
+++ 2.py
@@ -3,37 +3,29 @@ import time
 import matplotlib.pyplot as plt
 from statistics import mean
 
-# Параметры эксперимента
-N = 12  # 20 - 8 = 12
-max_n = 10**2 * N  # максимальный размер матрицы (квадратный корень от предыдущего max_n)
-step = 1*N  # шаг для размера матрицы
-num_runs = 5  # уменьшили количество запусков, так как умножение матриц требует больше времени
+N = 12 
+max_n = 10**2 * N
+step = 1*N
+num_runs = 5   
 
-# Списки для хранения результатов
 n_values = []
 times = []
 
-# Проводим эксперимент для разных размеров матриц
 for n in range(1, max_n + 1, step):
     
-    # Генерируем две случайные матрицы размера n x n
     A = np.random.rand(n, n)
     B = np.random.rand(n, n)
     print(f"Обрабатывается размер матрицы = {n}x{n}")
     run_times = []
     
-    # Несколько запусков для усреднения
     for run in range(num_runs):
-        # Замер времени умножения матриц
         start_time = time.time()
         
-        # Умножение матриц - исследуемая операция
         C = np.dot(A, B)
         
         end_time = time.time()
         run_times.append(end_time - start_time)
     
-    # Сохраняем среднее время
     n_values.append(n)
     times.append(mean(run_times))
 
